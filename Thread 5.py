7be471e6caf986bb55e293ab7d85631372bd9edd
Alexis Schoenn <alexis.schoenn@uha.fr>
2022-11-10 08:54:04 +0100

    Initial commit

---
+++ Thread 5.py
@@ -0,0 +1,20 @@
+import threading
+import time
+
+T=[]
+
+def task(i):
+    print(f"Task {i} starts")
+    time.sleep(1)
+    print(f"Task {i} ends")
+if __name__ == '__main__':
+    start = time.perf_counter()
+
+    for i in range (100):
+        T.append(threading.Thread(target=task, args=[i]))
+    for i in range(len(T)):
+        T[i].start()
+    for i in range(len(T)):
+        T[i].join()
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
